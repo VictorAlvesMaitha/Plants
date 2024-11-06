08b1c70e36d08ebb85f3fe0bbe9e69c0d7750b0e
unknown <victor.alves@maitha.com.br>
2024-11-06 14:43:47 -0300

    testing Code Rabbit

--- fibo.py
+++ fibo.py
@@ -6,6 +6,8 @@ def fibonacci(n: int) -> float:
     fib_seq = [0, 1]  # Initialize with the first two Fibonacci numbers
     for i in range(2, n):  # Loop to generate the sequence
         next_value = fib_seq[i] + fib_seq[i-1]  
+        next_value += 1
         fib_seq.append(next_value)
+
     
     return fib_seq  
